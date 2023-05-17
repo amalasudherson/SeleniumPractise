0843d98a975f50e13d56977ebcc35c092532ace1
rathinreddy <amala.r.sudherson@gmail.com>
2023-05-17 19:46:53 -0400

    Selenium9CreateDeleteproduct1working
    Selenium9CreateDeleteproduct1working

---
+++ SeleniumFrameWork9/src/test/java/Campaigns/CreateCampaigns.java
@@ -0,0 +1,10 @@
+package Campaigns;
+
+public class CreateCampaigns {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- SeleniumFrameWork9/src/test/java/Product/CreateproductAndDeleteProduct.java
+++ SeleniumFrameWork9/src/test/java/Product/CreateproductAndDeleteProduct.java
@@ -1,6 +1,7 @@
 package Product;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Properties;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -15,7 +16,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class CreateProduct {
+public class CreateproductAndDeleteProduct {
 
 	public static void main(String[] args) throws Throwable {
 
@@ -40,41 +41,32 @@ public class CreateProduct {
 		driver.findElement(By.name("user_name")).sendKeys(USERNAME);
 		driver.findElement(By.name("user_password")).sendKeys(PASSWORD);
 		driver.findElement(By.id("submitButton")).click();
-		
+
 		driver.findElement(By.xpath("//a[text()='Products']")).click();
 		driver.findElement(By.xpath("//img[@alt='Create Product...']")).click();
-		
-		Random ran=new Random();
+
+		Random ran = new Random();
 		int RanNum = ran.nextInt();
-		
+
 		FileInputStream fes = new FileInputStream("./src/test/resources/ExcelSheetData.xlsx");
 		Workbook book = WorkbookFactory.create(fes);
 		Sheet sheet = book.getSheet("Product");
-		Row row = sheet.getRow(0);
-		Cell cell = row.getCell(0);
+		Row row = sheet.getRow(2);
+		Cell cell = row.getCell(1);
 		String productdata = cell.getStringCellValue();
- 
-		
-        driver.findElement(By.name("productname")).sendKeys(productdata);
 
-        
-        driver.findElement(By.xpath("//input[@title='Save [Alt+S]']")).click();
-        String actdata = driver.findElement(By.cssSelector("span.lvtHeaderText")).getText();
-        if(actdata.contains(productdata)) 
-        	
-        {
-        	System.out.println("Validation pass");
-        	
-        }
-        else
-        {
-        	System.out.println("Validation fail");
-        	
-        }
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//img[@src='themes/softed/images/user.PNG']")).click();
-        driver.findElement(By.linkText("Sign Out")).click();
-        
+		driver.findElement(By.name("productname")).sendKeys(productdata);
+
+		driver.findElement(By.xpath("//input[@title='Save [Alt+S]']")).click();
+		driver.findElement(By.xpath("//input[@title='Delete [Alt+D]']")).click();
+
+		Thread.sleep(2000);
+		driver.switchTo().alert().accept();
+
+		driver.findElement(By.xpath("//img[@src='themes/softed/images/user.PNG']")).click();
+		driver.findElement(By.linkText("Sign Out")).click();
+		driver.close();
+
 	}
 
 }

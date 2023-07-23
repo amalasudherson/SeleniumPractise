5282b06737f708f1ac94d2ac86c1c195aa25d6ad
Amala Sudherson <amala.r.sudherson@gmail.com>
2023-07-23 13:23:36 -0400

    DeleteProduct1Working
    DeleteProduct1Working

--- PracticeMaven1/src/test/java/Product/DeleteProduct1.java
+++ PracticeMaven1/src/test/java/Product/DeleteProduct1.java
@@ -1,7 +1,7 @@
 package Product;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Properties;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -11,39 +11,36 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-import Generic_Utility.Excel_Utility;
-import Generic_Utility.Java_Utility;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class DeleteProduct {
+public class DeleteProduct1 {
 
 	public static void main(String[] args) throws Throwable {
 
 		String key = "webdriver.chrome.driver";
-		String Value = ".\\src\\main\\resources\\chromedriver.exe";
+		String Value = "./src/main/resources/chromedriver.exe";
 		System.setProperty(key, Value);
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		
-	
+
 		FileInputStream fis = new FileInputStream("./src/test/resources/PropertyFileData.properties");
 		Properties pro = new Properties();
 		pro.load(fis);
-
 		String URL = pro.getProperty("url");
 		String USERNAME = pro.getProperty("username");
 		String PASSWORD = pro.getProperty("password");
+
 		driver.get(URL);
-		// driver.get("http://localhost:8888/");
-		driver.findElement(By.name("user_name")).sendKeys(USERNAME);
+		driver.findElement(By.xpath("//input[@name='user_name']")).sendKeys(USERNAME);
 		driver.findElement(By.name("user_password")).sendKeys(PASSWORD);
 		driver.findElement(By.id("submitButton")).click();
 
@@ -51,44 +48,59 @@ public class DeleteProduct {
 		driver.findElement(By.xpath("//img[@alt='Create Product...']")).click();
 
 		Random ran = new Random();
-		int RanNum = ran.nextInt(1000);
+		int RanNUm = ran.nextInt(1000);
 
 		FileInputStream fes = new FileInputStream("./src/test/resources/ExcelSheetData.xlsx");
 		Workbook book = WorkbookFactory.create(fes);
-		Sheet sheet = book.getSheet("Product");
+		Sheet sheet = book.getSheet("product");
 		Row row = sheet.getRow(0);
-		Cell cell = row.getCell(0);
-		String productdata = cell.getStringCellValue() + RanNum;
+		Cell cel = row.getCell(0);
+		String productdata = cel.getStringCellValue() + RanNUm;
 
 		driver.findElement(By.name("productname")).sendKeys(productdata);
+		System.out.println(productdata);
 
 		driver.findElement(By.xpath("//input[@title='Save [Alt+S]']")).click();
-		driver.findElement(By.xpath("(//a[text()='Products'])[1]")).click();
+		System.out.println(1);
 
-		driver.findElement(By.xpath(
-				"//table[@class='lvt small']/tbody//td//a[text()='" + productdata + "']/../preceding-sibling::td[2]"))
-				.click();
+		driver.findElement(By.xpath("(//a[text()='Products'])[1]")).click();
+		System.out.println(2);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//table[@class='lvt small']/tbody//td//a[text()='" + productdata + "']/../preceding-sibling::td[2]")).click();
+		System.out.println(3);
+		Thread.sleep(2000);
+
 		driver.findElement(By.xpath("//input[@value='Delete']")).click();
-		Alert alert = driver.switchTo().alert();
-		alert.accept();
+		Thread.sleep(2000);
+
+		Alert alt = driver.switchTo().alert();
+		alt.accept();
+		List<WebElement> list = driver
+				.findElements(By.xpath("(//table[@class='lvt small']/tbody/tr/td[3])[position()>1]"));
+
+		boolean flag = false;
+		for (WebElement wb : list) {
+
+			String data = wb.getText();
+			if (data.contains(productdata)) {
+				flag = true;
+				break;
+			}
+		}
+		if (flag) {
+			System.out.println("deleted");
+			System.out.println(4);
+
+		} else {
+			System.out.println("not deleted");
+			
+			
+			
+			 driver.findElement(By.xpath("//img[@src='themes/softed/images/user.PNG']")).click();
+			 System.out.println(5);
+		        driver.findElement(By.linkText("Sign Out")).click();
+		}
+
 	}
 
 }
-
-/*
- * String actdata =
- * driver.findElement(By.cssSelector("span.lvtHeaderText")).getText();
- * if(actdata.contains(productdata))
- * 
- * { System.out.println("Validation pass");
- * 
- * } else { System.out.println("Validation fail");
- * 
- * } driver.findElement(By.xpath("//input[@title='Delete [Alt+D]']")).click();
- * 
- * Alert alert = driver.switchTo().alert(); alert.accept();
- * 
- * Thread.sleep(2000);
- * driver.findElement(By.xpath("//img[@src='themes/softed/images/user.PNG']")).
- * click(); driver.findElement(By.linkText("Sign Out")).click(); } }
- */
